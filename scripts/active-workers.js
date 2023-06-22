daa74e3b9ba4169fdf561091dacc3fc9ab200b55
dzhelezov <dzhelezov@gmail.com>
2023-06-22 14:55:31 +0200

    support multiple networks for active-workers

--- scripts/active-workers.js
+++ scripts/active-workers.js
@@ -2,12 +2,24 @@ const {ethers, utils} = require("ethers");
 const bs58 = require('bs58');
 const fs = require('fs')
 
-const wrDeployArtifact = JSON.parse(fs.readFileSync("./deployments/localhost/WorkerRegistration.json", "utf8"));
+const RPC_PROVIDER_URL = process.env.RPC_PROVIDER_URL || 'http://127.0.0.1:8545'
+console.log(`Using RPC: ${RPC_PROVIDER_URL}`)
+
+const NETWORK_NAME=process.env.NETWORK_NAME || 'localhost'
+
+const SUPPORTED_NETWORKS = ['localhost', 'arbitrum-goerli']
+
+if (!SUPPORTED_NETWORKS.includes(NETWORK_NAME)) {
+  console.error(`Unsupported network ${NETWORK_NAME}. Supported networks: ${SUPPORTED_NETWORKS}`);
+  process.exit(1);
+}
+
+const wrDeployArtifact = JSON.parse(fs.readFileSync(`./deployments/${NETWORK_NAME}/WorkerRegistration.json`, "utf8"));
 const workerRegistrationAddress = wrDeployArtifact.address;
 const workerRegistrationABI = wrDeployArtifact.abi;
 
 // Connect to the network
-const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
+const provider = new ethers.providers.JsonRpcProvider(RPC_PROVIDER_URL);
 // Connect to the WorkerRegistration contract
 const workerRegistrationContract = new ethers.Contract(workerRegistrationAddress, workerRegistrationABI, provider);
 
