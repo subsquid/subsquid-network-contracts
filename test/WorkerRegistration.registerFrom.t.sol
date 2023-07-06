d4e84996f8632f9fb2ca9425c541c4a2467f3f20
ivan <ivanruch@gmail.com>
2023-07-06 10:21:27 +0200

    Add tests for register

--- test/WorkerRegistration.registerFrom.t.sol
+++ test/WorkerRegistration.registerFrom.t.sol
@@ -25,4 +25,24 @@ contract WorkerRegistrationRegisterFromTest is WorkerRegistrationTest {
         vm.expectRevert("Worker already registered");
         workerRegistration.registerFrom(worker, workerId);
     }
+
+    function testIncrementsIdForNextWorker() public {
+        token.approve(address(workerRegistration), workerRegistration.BOND_AMOUNT() * 2);
+
+        workerRegistration.registerFrom(address(123), workerId);
+        workerRegistration.registerFrom(worker, workerId);
+        assertEq(workerRegistration.workerIds(worker), 2);
+    }
+
+    function testCorrectlyCreatesWorkerStruct() public {
+        workerRegistration.registerFrom(worker, workerId);
+
+        WorkerRegistration.Worker memory workerStruct = workerRegistration.getWorkerByIndex(0);
+        assertEq(workerStruct.account, worker);
+        assertEq(workerStruct.peerId[0], workerId[0]);
+        assertEq(workerStruct.peerId[1], workerId[1]);
+        assertEq(workerStruct.bond, workerRegistration.BOND_AMOUNT());
+        assertEq(workerStruct.registeredAt, block.number + block.number % 2);
+        assertEq(workerStruct.deregisteredAt, 0);
+    }
 }
