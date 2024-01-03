7daabf5953ef120ffa4cce0425712bfb3b0717e4
ivan <ivanruch@gmail.com>
2024-01-03 17:14:16 +0100

    Fix docker build

--- packages/contracts/spinup-testnet.sh
+++ packages/contracts/spinup-testnet.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+ls -la .
+ls -la lib
+ls -la lib/openzeppelin-contracts
+ls -la lib/openzeppelin-contracts/contracts
+ls -la lib/openzeppelin-contracts/contracts/token
+ls -la lib/openzeppelin-contracts/contracts/token/ERC20
+
+
+forge build
 forge script script/Deploy.s.sol --broadcast --json --rpc-url $RPC_URL
 forge script script/PreparePlayground.s.sol --tc PreparePlayground --broadcast --json --rpc-url $RPC_URL
 echo "Deployed contract addresses"
