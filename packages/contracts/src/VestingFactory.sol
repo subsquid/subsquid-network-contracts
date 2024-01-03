99b51bae96c7bd6c505ac39d037ed8753d11e549
ivan <ivanruch@gmail.com>
2024-01-03 13:37:10 +0100

    Remove deprecated worker registration docker action

--- packages/contracts/src/VestingFactory.sol
+++ packages/contracts/src/VestingFactory.sol
@@ -9,7 +9,13 @@ contract VestingFactory {
   IERC20 public immutable token;
   IRouter public immutable router;
 
-  event VestingCreated(address indexed vesting, address indexed beneficiary, uint64 startTimestamp, uint64 durationSeconds, uint256 expectedTotalAmount);
+  event VestingCreated(
+    address indexed vesting,
+    address indexed beneficiary,
+    uint64 startTimestamp,
+    uint64 durationSeconds,
+    uint256 expectedTotalAmount
+  );
 
   constructor(IERC20 _token, IRouter _router) {
     token = _token;
@@ -23,7 +29,9 @@ contract VestingFactory {
     uint64 durationSeconds,
     uint256 expectedTotalAmount
   ) external returns (address) {
-    address vesting = address(new SubsquidVesting(token, router, beneficiaryAddress, startTimestamp, durationSeconds, expectedTotalAmount));
+    address vesting = address(
+      new SubsquidVesting(token, router, beneficiaryAddress, startTimestamp, durationSeconds, expectedTotalAmount)
+    );
     emit VestingCreated(vesting, beneficiaryAddress, startTimestamp, durationSeconds, expectedTotalAmount);
     return vesting;
   }
