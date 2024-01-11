235b9e76d19986a4a2991d8f54b88ebd5b071e14
ivan <ivanruch@gmail.com>
2024-01-11 13:02:27 +0100

    Prepare docker with all contracts

--- packages/contracts/script/RegisterWorker.s.sol
+++ packages/contracts/script/RegisterWorker.s.sol
@@ -6,9 +6,17 @@ import "../src/WorkerRegistration.sol";
 
 contract RegisterWorker is Script {
   function run() public {
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    bytes memory peerId = vm.envBytes("WORKER_ID");
-    WorkerRegistration workerRegistration = WorkerRegistration(vm.envAddress("WORKER_REGISTRATION"));
+    uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0));
+    bytes memory peerId = vm.envOr("WORKER_ID", bytes(""));
+    WorkerRegistration workerRegistration = WorkerRegistration(vm.envOr("WORKER_REGISTRATION", address(0x7Bf0B1ee9767eAc70A857cEbb24b83115093477F)));
+    if (deployerPrivateKey == 0) {
+      console2.log("PRIVATE_KEY env var is required");
+      return;
+    }
+    if (peerId.length == 0) {
+      console2.log("WORKER_ID env var is required");
+      return;
+    }
     IERC20 token = workerRegistration.tSQD();
     vm.startBroadcast(deployerPrivateKey);
     token.approve(address(workerRegistration), workerRegistration.bondAmount());
