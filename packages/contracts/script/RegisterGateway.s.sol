78e559fee1e2f2679e74f135e776982ce55a68ba
ivan <ivanruch@gmail.com>
2024-01-11 14:37:38 +0100

    Make build in dockerfile

--- packages/contracts/script/RegisterGateway.s.sol
+++ packages/contracts/script/RegisterGateway.s.sol
@@ -6,14 +6,28 @@ import "../src/GatewayRegistry.sol";
 
 contract RegisterGateway is Script {
   function run() public {
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-    vm.startBroadcast(deployerPrivateKey);
-    bytes memory peerId = vm.envBytes("GATEWAY_ID");
-    GatewayRegistry gatewayReg = GatewayRegistry(vm.envAddress("GATEWAY_REGISTRY"));
+    uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0));
+    bytes memory peerId = vm.envOr("GATEWAY_ID", bytes(""));
+    if (deployerPrivateKey == 0) {
+      console2.log("PRIVATE_KEY env var is required");
+      return;
+    }
+    if (peerId.length == 0) {
+      console2.log("GATEWAY_ID env var is required");
+      return;
+    }
+    uint256 stakeAmount = vm.envOr("STAKE_AMOUNT", uint256(100)) * 1 ether;
+    uint256 duration = vm.envOr("STAKE_DURATION", uint256(180)) * 1 days;
+    GatewayRegistry gatewayReg = GatewayRegistry(vm.envOr("GATEWAY_REGISTRY", address(0xC168fD9298141E3a19c624DF5692ABeeb480Fb94)));
     IERC20 token = gatewayReg.token();
-    gatewayReg.register(peerId);
-    token.approve(address(gatewayReg), 100 ether);
-    gatewayReg.stake(100 ether, 180 days);
+    vm.startBroadcast(deployerPrivateKey);
+    if (gatewayReg.peerIds(msg.sender).length == 0) {
+      gatewayReg.register(peerId);
+    } else {
+      console2.log("Gateway already registered");
+    }
+    token.approve(address(gatewayReg), stakeAmount);
+    gatewayReg.stake(stakeAmount, duration);
     vm.stopBroadcast();
 
     console2.log("Gateway has", gatewayReg.computationUnits(vm.addr(deployerPrivateKey)), "CUs");
