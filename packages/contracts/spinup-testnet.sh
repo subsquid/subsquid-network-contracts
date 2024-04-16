61eb7f5b1eeae5059cd90bd148d48660bb56b1b5
ivan <ivanruch@gmail.com>
2024-04-16 22:03:40 +0200

    Add multicall address to logs

--- packages/contracts/spinup-testnet.sh
+++ packages/contracts/spinup-testnet.sh
@@ -5,6 +5,7 @@ forge script script/Deploy.s.sol --broadcast --json --rpc-url $RPC_URL
 forge script script/PreparePlayground.s.sol --tc PreparePlayground --broadcast --json --rpc-url $RPC_URL
 echo "Deployed contract addresses"
 jq '[.transactions[] | select(.transactionType == "CREATE")] | map({contractName, contractAddress})' ./broadcast/Deploy.s.sol/31337/run-latest.json
+jq '[.transactions[] | select(.transactionType == "CREATE")] | map({contractName, contractAddress})' ./broadcast/PreparePlayground.s.sol/31337/run-latest.json
 
 echo "Register workers"
 
