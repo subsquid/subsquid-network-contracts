294c8f739bf5b2b653d3b1c59131d54ec02aebad
ivan <ivanruch@gmail.com>
2024-04-17 14:14:13 +0200

    fix resupporting/unsupporting same workers in subequal strategy

--- packages/contracts/src/gateway-strategies/SubequalStrategy.sol
+++ packages/contracts/src/gateway-strategies/SubequalStrategy.sol
@@ -11,8 +11,8 @@ contract SubequalStrategy is IGatewayStrategy {
   mapping(address gatewayOperator => mapping(uint256 workerId => bool)) public isWorkerSupported;
   mapping(address gatewayOperator => uint256) public workerCount;
 
-  event WorkerSupported(address gatewayOperator, uint256 workerId);
-  event WorkerUnsupported(address gatewayOperator, uint256 workerId);
+  event WorkerSupported(address indexed gatewayOperator, uint256 indexed workerId);
+  event WorkerUnsupported(address indexed gatewayOperator, uint256 indexed workerId);
 
   constructor(IRouter _router, IGatewayRegistry _gatewayRegistry) {
     router = _router;
@@ -21,18 +21,22 @@ contract SubequalStrategy is IGatewayStrategy {
 
   function supportWorkers(uint256[] calldata workerIds) external {
     for (uint256 i = 0; i < workerIds.length; i++) {
+      if (isWorkerSupported[msg.sender][workerIds[i]]) {
+        continue;
+      }
       isWorkerSupported[msg.sender][workerIds[i]] = true;
+      workerCount[msg.sender]++;
       emit WorkerSupported(msg.sender, workerIds[i]);
     }
-    workerCount[msg.sender] += workerIds.length;
   }
 
   function unsupportWorkers(uint256[] calldata workerIds) external {
     for (uint256 i = 0; i < workerIds.length; i++) {
+      require(isWorkerSupported[msg.sender][workerIds[i]], "Worker is not supported");
       isWorkerSupported[msg.sender][workerIds[i]] = false;
+      workerCount[msg.sender]--;
       emit WorkerUnsupported(msg.sender, workerIds[i]);
     }
-    workerCount[msg.sender] -= workerIds.length;
   }
 
   function computationUnitsPerEpoch(bytes calldata gatewayId, uint256 workerId) external view returns (uint256) {
